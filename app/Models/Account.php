e7f30e2abefcb9586e1f22b36163ed7f6f216bd1
Haaragard <haaragard@gmail.com>
2023-08-07 22:44:48 -0300

    feat: add account model

---
+++ app/Models/Account.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\HasUuids;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Account extends Model
+{
+    use HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'balance',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'timestamp',
+        'updated_at' => 'timestamp',
+    ];
+
+    /**
+     * Get the account balance.
+     */
+    protected function balance(): Attribute
+    {
+        return Attribute::make(
+            get: fn (int $value) => ((float) $value) / 100,
+            set: fn (int $value) => (int) ($value * 100),
+        );
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+}
