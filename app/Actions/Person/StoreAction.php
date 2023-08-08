0b4e9a7970b40fcc1ba7e98bb88f63beaae0a491
Haaragard <haaragard@gmail.com>
2023-08-08 20:15:26 -0300

    feat: add person store action

---
+++ app/Actions/Person/StoreAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Actions\Person;
+
+use App\Actions\Traits\CreateUserFromUserable;
+use App\Actions\User\StoreAction as UserStoreAction;
+use App\Dtos\Person\StoreDto;
+use App\Dtos\User\StoreDto as UserStoreDto;
+use App\Models\Person;
+use App\Services\Contracts\PersonServiceContract;
+use Illuminate\Support\Facades\DB;
+
+class StoreAction
+{
+    use CreateUserFromUserable;
+
+    /**
+     * @param PersonServiceContract $service
+     * @param UserStoreAction $storeUserAction
+     */
+    public function __construct(
+        private PersonServiceContract $service,
+        protected UserStoreAction $storeUserAction
+    ) { }
+
+    /**
+     * @param StoreDto $dto
+     * @return Person
+     */
+    public function execute(StoreDto $dto, UserStoreDto $storeUserDto): Person
+    {
+        $person = DB::transaction(function () use (&$dto, &$storeUserDto) {
+            $person = $this->createPerson($dto);
+            $this->createUser($person, $storeUserDto);
+
+            return $person;
+        });
+
+        return $person;
+    }
+
+    /**
+     * @param StoreDto $dto
+     * @return Person
+     */
+    private function createPerson(StoreDto $dto): Person
+    {
+        return $this->service->create($dto);
+    }
+}
