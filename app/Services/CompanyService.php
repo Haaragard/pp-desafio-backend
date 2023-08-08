b2ed5bcb67cb9d0b42ccd3efd6d57c16897b94e3
Haaragard <haaragard@gmail.com>
2023-08-08 20:12:26 -0300

    feat: add company service create method

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Dtos\Company\StoreDto;
+use App\Models\Company;
 use App\Repositories\Contracts\CompanyRepositoryContract;
 use App\Services\Contracts\CompanyServiceContract;
 
@@ -12,4 +14,12 @@ class CompanyService implements CompanyServiceContract
      */
     public function __construct(private CompanyRepositoryContract $repository)
     { }
+
+    /**
+     * @inheritDoc
+     */
+    public function create(StoreDto $dto): Company
+    {
+        return $this->repository->create($dto->toArray());
+    }
 }
--- app/Services/Contracts/CompanyServiceContract.php
+++ app/Services/Contracts/CompanyServiceContract.php
@@ -2,7 +2,14 @@
 
 namespace App\Services\Contracts;
 
+use App\Dtos\Company\StoreDto;
+use App\Models\Company;
+
 interface CompanyServiceContract
 {
-    //
+    /**
+     * @param StoreDto $dto
+     * @return Company
+     */
+    public function create(StoreDto $dto): Company;
 }
