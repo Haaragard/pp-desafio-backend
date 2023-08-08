5033743c325f01149025de44048bd025486ff2f9
Haaragard <haaragard@gmail.com>
2023-08-08 20:17:21 -0300

    feat: add person create route

--- routes/v1/index.php
+++ routes/v1/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PersonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::prefix('person')->name('.person')->group(function () {
+    Route::post('/', [PersonController::class, 'store'])->name('.create');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
