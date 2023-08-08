c37a7b1a8e49dbb0ed02a949d0ce1ece2500a3d2
Haaragard <haaragard@gmail.com>
2023-08-07 22:21:32 -0300

    feat: add person model

---
+++ app/Models/Person.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Contracts\Userable;
+use App\Models\Traits\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+class Person extends Model implements Userable
+{
+    use HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'cpf',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'timestamp',
+        'updated_at' => 'timestamp',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function getCredential(): string
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * Get the user.
+     * @return MorphOne
+     */
+    public function user(): MorphOne
+    {
+        return $this->morphOne(User::class, 'userable');
+    }
+}
