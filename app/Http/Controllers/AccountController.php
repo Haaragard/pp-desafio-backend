8c564815a195ae9f15a1f3f189bee002ce31112a
Haaragard <haaragard@gmail.com>
2023-08-09 23:44:50 -0300

    feat: add account controller withdraw method

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Actions\Account\DepositAction;
+use App\Actions\Account\WithdrawAction;
 use App\Dtos\Account\DepositDto;
+use App\Dtos\Account\WithdrawDto;
 use App\Http\Requests\Account\DepositRequest;
+use App\Http\Requests\Account\WithdrawRequest;
 use Illuminate\Http\Response;
 
 class AccountController extends Controller
@@ -27,4 +30,21 @@ class AccountController extends Controller
         return response(status: $statusResponse);
     }
 
+    /**
+     * @param WithdrawRequest $request
+     * @param WithdrawAction $action
+     * @return Response
+     */
+    public function withdraw(WithdrawRequest $request, WithdrawAction $action): Response
+    {
+        $withdrawDto = new WithdrawDto($request->validated('amount'));
+
+        $successOnWithdraw = $action->execute($withdrawDto);
+
+        $statusResponse = ($successOnWithdraw)
+            ? Response::HTTP_CREATED
+            : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        return response(status: $statusResponse);
+    }
 }
