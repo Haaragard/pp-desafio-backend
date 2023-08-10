2255b12ee4e56e5dfd6f5907995597a61a277083
Haaragard <haaragard@gmail.com>
2023-08-10 00:29:59 -0300

    feat: add transaction repository create transaction method

--- app/Repositories/Contracts/TransactionRepositoryContract.php
+++ app/Repositories/Contracts/TransactionRepositoryContract.php
@@ -2,7 +2,16 @@
 
 namespace App\Repositories\Contracts;
 
+use App\Models\Account;
+use App\Models\Transaction;
+
 interface TransactionRepositoryContract extends BaseRepositoryContract
 {
-    //
+    /**
+     * @param Account $payer
+     * @param Account $payee
+     * @param float $amount
+     * @return Transaction
+     */
+    public function createTransaction(Account $payer, Account $payee, float $amount): Transaction;
 }
--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Models\Account;
 use App\Models\Transaction;
 use App\Repositories\Contracts\TransactionRepositoryContract;
 
@@ -13,5 +14,26 @@ class TransactionRepository extends BaseRepository implements TransactionReposit
     public function __construct(Transaction $model)
     {
         parent::__construct($model);
+    }
+
+    /**
+     * @param Account $payer
+     * @param Account $payee
+     * @param float $amount
+     * @return Transaction
+     */
+    public function createTransaction(Account $payer, Account $payee, float $amount): Transaction
+    {
+        /**
+         * @var Transaction
+         */
+        $transaction = new $this->model;
+
+        $transaction->payer()->associate($payer);
+        $transaction->payee()->associate($payee);
+        $transaction->amount = (int) ($amount * 100);
+        $transaction->save();
+
+        return $transaction;
     }
 }
