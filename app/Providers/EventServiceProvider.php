d9f5ae8ceec0c963524ceb07f9842fa0ed13a6d0
Haaragard <haaragard@gmail.com>
2023-08-10 22:51:21 -0300

    feat: register new events and listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Events\TransactionApproved;
+use App\Events\TransactionReproved;
+use App\Listeners\ApproveTransaction;
+use App\Listeners\ReproveTransaction;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +20,12 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        TransactionApproved::class => [
+            ApproveTransaction::class,
+        ],
+        TransactionReproved::class => [
+            ReproveTransaction::class,
         ],
     ];
 
